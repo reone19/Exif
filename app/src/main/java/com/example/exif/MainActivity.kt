f8429b0e8e0c8853da3e992d6ff6aea3d45c268e
nagi <ymxlinkinparkxlc@gmail.com>
2021-06-28 05:33:59 +0900

    RealmによるCRUDテスト

--- app/src/main/java/com/example/exif/MainActivity.kt
+++ app/src/main/java/com/example/exif/MainActivity.kt
@@ -1,31 +1,136 @@
 package com.example.exif
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.exif.databinding.ActivityMainBinding
 import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var item: Photo
+    private lateinit var binding: ActivityMainBinding
     private lateinit var realm: Realm
+    // private val args: MainActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        // インスタンス取得 (初期化なのでDB処理を行うときはonCreateメソッドに必ず書く)
         realm = Realm.getDefaultInstance()
 
-        val state = intent.getSerializableExtra("hoge")
-        if (state is Photo) {
-            item = state
-            createOrUpdate()
+
+        //削除ボタンを押したときにdeleteExecuteを実行して該当レコードを削除
+        binding.deleteBtn.setOnClickListener { deleteExecute(it) }
+    }
+
+
+    // Realmのインスタンスを破棄してリソース開放
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+
+    // Create
+    private fun insertExecute(view: View) {
+        realm.executeTransaction { db: Realm ->
+            // idのインクリメント処理はこれを使うか、共有プリファレンスに常に最大のid番号を登録しておき、
+            // その値プラス1をする
+            val maxId = db.where<Photo>().max("photoID")
+            val nextId = (maxId?.toLong() ?: 0L) + 1L
+            val photo = db.createObject<Photo>(nextId)
+
+            // 各カラムにコンテンツプロバイダから取得した情報をセット(コンテンツプロバイダ周りはまだ)
+            photo?.photoName = binding.NameEdit.text.toString()
+            photo?.photoURL = binding.URLEdit.text.toString()
+        }
+
+
+        // 簡易Insert文
+        // private fun insertExecute(view: View)
+        // realm.executeTransaction {
+        // val tmp1 = Photo(1,”名前”,"URL", "NULL")
+        // realm.insert(tmp1)
+        // }
+    }
+
+
+    // Read
+    private fun readExecute(view: View) {
+        // ログに出力する簡易版
+        // var read = realm.where(Photo::class.java)
+        // .equalTo("photo_name", "*.jpg")
+        // .findAll()
+        // ログに出力？
+        // Log.d(TAG, read.toString())
+
+
+        // id番号に一致しているカラムを取得
+        val photo = realm.where<Photo>()
+            .equalTo("photoId", "id番号")
+            .findFirst()
+
+        // DBに保存されている各レコードをアプリケーションのXMLにセット
+        binding.nameEdit.setText(photo?.photoName)
+        binding.urlEdit.setText(photo?.photoURL)
+    }
+
+
+    // Update
+    private fun updateExecute(view: View) {
+//        val photo = realm.where<Photo>()
+//            .equalTo("photoId", "id番号")
+//            .findFirst()
+//        binding.titleEdit.setText(photo?.title)
+//        binding.detailEdit.setText(photo?.detail)
+//
+//        // 保存ボタン
+        // binding.save.setOnClickListener{ saveExecute(it) }
+
+
+        realm.executeTransaction { db: Realm ->
+            val photo = realm.where<Photo>()
+                .equalTo("photoId", "id番号")
+                .findFirst()
+
+            // XMLに入力されたテキストをDBに保存して更新
+            photo?.photoName = binding.photoNameEdit.text.toString()
+            photo?.photoURL = binding.photoURLEdit.text.toString()
+
+
+//       var update = realm.where(Photo::class.java)
+//            .equalTo("photo_name", "*.jpg")
+//            .findAll()
+//        realm.executeTransaction {
+//            update.updateAllFromRealm()
+//
+//        }
+
         }
     }
 
-    private fun createOrUpdate() {
-        val photo = Photo(item.photoId, item.photoName, item.photoURL, item.deleteDate)
-        realm.beginTransaction()
-        realm.copyToRealmOrUpdate(photo)
-        realm.commitTransaction()
+
+    // Delete
+    private fun deleteExecute(view: View) {
+//        var delete = realm.where(Photo::class.java)
+//            .equalTo("photo_name", "*.jpg")
+//            .findAll()
+        //削除
+//        realm.executeTransaction {
+//            delete.deleteAllFromRealm()
+
+
+        realm.executeTransaction { db: Realm ->
+            db.where<Photo>().equalTo("id", "id番号")
+                ?.findFirst()
+                ?.deleteFromRealm()
+        }
     }
 }
--- app/src/main/java/com/example/exif/PhotoAdapter.kt
+++ app/src/main/java/com/example/exif/PhotoAdapter.kt
@@ -1,7 +1,39 @@
 //package com.example.exif
 //
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
 //import io.realm.OrderedRealmCollection
 //import io.realm.RealmRecyclerViewAdapter
 //
-//class PhotoAdapter(data: OrderedRealmCollection<Photo>) : RealmRecyclerViewAdapter<Photo, PhotoAdapter.ViewHolder>(data, true) {
+//class PhotoAdapter(data: OrderedRealmCollection<Photo>) :
+//    RealmRecyclerViewAdapter<Photo, PhotoAdapter.ViewHolder>(data, true) {
+//
+//    init {
+//        setHasStableIds(true)
+//    }
+//
+//    class ViewHolder(cell: View) : RecyclerView.ViewHolder(cell) {
+//        val deleteDate: TextView = cell.findViewById(android.R.id.text1)
+//        val photoName: TextView = cell.findViewById(android.R.id.text2)
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoAdapter.ViewHolder {
+//        val inflater = LayoutInflater.from(parent.context)
+//        val view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+//        return ViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: PhotoAdapter.ViewHolder, position: Int) {
+//        val photo: Photo? = getItem(position)
+//        holder.deleteDate = photo?.deleteDate
+//        holder.photoName.text = photo?.photoName
+//    }
+//
+//    override fun getItemId(position: Int): Long {
+//        return getItem(position)?.id ?: 0
+//    }
+//
 //}
