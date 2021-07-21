7a616109d38c4057e39af425a312c0c9495a05ca
reone19 <reone1999@gmail.com>
2021-07-21 17:33:19 +0900

    吉田が画像タップした時の画像拡大画面をギャラリーと結合した。 ORCについてはOcrFragmantの外部ストレージから読み込んでるintentの所をギャラリーでタップした画像をOCRに読み込ませられるようにする。

--- app/src/main/java/com/example/exif/model/PhotoAdapter.kt
+++ app/src/main/java/com/example/exif/model/PhotoAdapter.kt
@@ -9,8 +9,6 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.exif.OcrFragment
-import com.example.exif.model.Image
 import com.example.exif.R
 import com.example.exif.YoshidanoYatu
 
