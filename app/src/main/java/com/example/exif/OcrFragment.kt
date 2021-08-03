bb9af0f5563743cc025ea35ae1bddb50ec01b882
nagi <ymxlinkinparkxlc@gmail.com>
2021-08-04 03:07:51 +0900

    OCRをimagePathから読み込めるようにした

--- app/src/main/java/com/example/exif/OcrFragment.kt
+++ app/src/main/java/com/example/exif/OcrFragment.kt
@@ -1,18 +1,16 @@
 package com.example.exif
 
-import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.exif.databinding.FragmentOcrBinding
+import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
 
@@ -32,23 +30,67 @@ class OcrFragment : Fragment() {
     ): View? {
         _binding = FragmentOcrBinding.inflate(inflater, container, false)
 
-        // 写真選択ボタンを押したとき
-        binding.button.setOnClickListener { view ->
-            _ocr = activity?.let { OCR(it.applicationContext) }
+        _ocr = activity?.let { OCR(it.applicationContext) }
 
-            if (view === view?.findViewById<View>(R.id.button)) {
-                val intent: Intent
-                if (Build.VERSION.SDK_INT < 19) {
-                    intent = Intent(Intent.ACTION_PICK)
-                    intent.action = Intent.ACTION_GET_CONTENT
-                } else {
-                    intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-                    intent.addCategory(Intent.CATEGORY_OPENABLE)
+//            if (view === view?.findViewById<View>(R.id.button)) {
+//                val intent: Intent
+//                if (Build.VERSION.SDK_INT < 19) {
+//                    intent = Intent(Intent.ACTION_PICK)
+//                    intent.action = Intent.ACTION_GET_CONTENT
+//                } else {
+//                    intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+//                    intent.addCategory(Intent.CATEGORY_OPENABLE)
+//                }
+//                intent.type = "image/*"
+//                startActivityForResult(intent, REQUEST_CODE_PICK_CONTENT)
+//            }
+
+        val ocrString: String
+        var bitmap: Bitmap? = null
+
+        val f: File = File(imagePath)
+        val uri = Uri.fromFile(f)
+
+        if (Build.VERSION.SDK_INT < 19) {
+            try {
+                val `in` = activity?.contentResolver?.openInputStream(uri!!)
+                bitmap = BitmapFactory.decodeStream(`in`)
+                try {
+                    `in`?.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
                 }
-                intent.type = "image/*"
-                startActivityForResult(intent, REQUEST_CODE_PICK_CONTENT)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+
+        } else {
+            try {
+                val parcelFileDescriptor = activity?.contentResolver?.openFileDescriptor(
+                    uri!!, "r"
+                )
+                if (parcelFileDescriptor != null) {
+                    val fileDescriptor = parcelFileDescriptor.fileDescriptor
+                    bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                    parcelFileDescriptor.close()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
         }
+
+        if (bitmap != null) {
+//                val imageView = view?.findViewById<View>(R.id.image) as ImageView
+//                imageView.scaleType = ImageView.ScaleType.FIT_CENTER
+//                imageView.setImageBitmap(bitmap)
+            ocrString = activity?.applicationContext?.let { _ocr!!.getString(it, bitmap) }
+                .toString()
+        } else {
+            ocrString = "bitmap is null"
+        }
+
+        binding.ocrString.setText(ocrString)
+
         return binding.root
     }
 
@@ -57,59 +99,4 @@ class OcrFragment : Fragment() {
         _binding = null
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_PICK_CONTENT) {
-            val ocrString: String
-            if (resultCode == AppCompatActivity.RESULT_OK && data != null) {
-                var bitmap: Bitmap? = null
-
-                if (Build.VERSION.SDK_INT < 19) {
-                    try {
-                        val `in` = activity?.contentResolver?.openInputStream(data.data!!)
-                        bitmap = BitmapFactory.decodeStream(`in`)
-                        try {
-                            `in`?.close()
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        }
-                    } catch (e: FileNotFoundException) {
-                        e.printStackTrace()
-                    }
-
-                } else {
-                    val uri = data.data
-                    try {
-                        val parcelFileDescriptor = activity?.contentResolver?.openFileDescriptor(
-                            uri!!, "r"
-                        )
-                        if (parcelFileDescriptor != null) {
-                            val fileDescriptor = parcelFileDescriptor.fileDescriptor
-                            bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor)
-                            parcelFileDescriptor.close()
-                        }
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                }
-
-                if (bitmap != null) {
-                    val imageView = view?.findViewById<View>(R.id.image) as ImageView
-                    imageView.scaleType = ImageView.ScaleType.FIT_CENTER
-                    imageView.setImageBitmap(bitmap)
-                    ocrString = activity?.applicationContext?.let { _ocr!!.getString(it, bitmap) }.toString()
-                } else {
-                    ocrString = "bitmap is null"
-                }
-
-            } else {
-                ocrString = "something wrong?"
-            }
-            (view?.findViewById<View>(R.id.ocrString) as TextView).text = ocrString
-        }
-    }
-
-    companion object {
-        private const val REQUEST_CODE_PICK_CONTENT = 0
-    }
 }
