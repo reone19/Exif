a4f697c222d31218f4c7b737ac01f3d594488f63
nagi <ymxlinkinparkxlc@gmail.com>
2021-08-03 17:56:55 +0900

    Exifの直接編集を可能にした

--- app/src/main/java/com/example/exif/ExifFragment.kt
+++ app/src/main/java/com/example/exif/ExifFragment.kt
@@ -15,7 +15,6 @@ import androidx.fragment.app.Fragment
 import com.example.exif.databinding.FragmentExifBinding
 import java.io.File
 import java.io.IOException
-import java.io.InputStream
 
 
 class ExifFragment : Fragment() {
@@ -43,77 +42,86 @@ class ExifFragment : Fragment() {
         // Exif取得
         val f: File = File(imagePath)
         val uri = Uri.fromFile(f)
-        var `in`: InputStream? = null
 
         try {
-            `in` = activity?.contentResolver?.openInputStream(uri)
-            var exifInterface = ExifInterface(`in`!!)
+            activity?.contentResolver?.openInputStream(uri)?.use {
+                val exifInterface = ExifInterface(
+                    context?.contentResolver?.openFileDescriptor(
+                        uri,
+                        "rw",
+                        null
+                    )!!.fileDescriptor
+                )
 
-            // Exifの各値をここにセット
-            // <変数> = exifInterface.getAttribute(ExifInterface.<ExifのTAG>)
 
-            // 画像の高さ
-            var imageLength = exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH)
-            // 画像の横幅
-            var imageWidth = exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH)
-            // 画像のビットの深さ
-            var bitsPerSample = exifInterface.getAttribute(ExifInterface.TAG_BITS_PER_SAMPLE)
-            // 圧縮の種類
-            var compression = exifInterface.getAttribute(ExifInterface.TAG_COMPRESSION)
-            // 画像タイトル
-            var imageDescription =
-                exifInterface.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION)
-            // 画像方向
-            var imageOrientation = exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION)
-            // メーカ名
-            var maker = exifInterface.getAttribute(ExifInterface.TAG_MAKE)
-            //モデル名
-            var model = exifInterface.getAttribute(ExifInterface.TAG_MODEL)
-            // ロケーション
-            var stripOffsets = exifInterface.getAttribute(ExifInterface.TAG_STRIP_OFFSETS)
-            // GPSタグのバージョン
-            var gpsVersionID = exifInterface.getAttribute(ExifInterface.TAG_GPS_VERSION_ID)
-            // 経度
-            var gpsLatitude = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE)
-            // 緯度
-            var gpsLongitude = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)
-            // 原画像データの生成日時
-            var dateTimeOriginal =
-                exifInterface.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL)
-            // 更新日時
-            var dateTime = exifInterface.getAttribute(ExifInterface.TAG_DATETIME)
+                //
+                //　ここの値を自由に変更するとExifの直接編集ができているか確認できる
+                //
+                //
+                // exifInterface.setAttribute(ExifInterface.TAG_BITS_PER_SAMPLE, "12")
+                //
+                //　コメントアウトをはずして任意の画像のExifページを開く→該当のsetAttributeがsaveされる→
+                //　コメントアウトをつけて、更新されているか確認
+                //　DBからではなく、Exifから表示させているので、変更が保存されていればExifが更新できている
+                // ことの証明になる
 
-            // データセット
-            binding.imageLength.setText(imageLength)
-            binding.imageWidth.setText(imageWidth)
-            binding.bitsPerSample.setText(bitsPerSample)
-            binding.compression.setText(compression)
-            binding.imageDescription.setText(imageDescription)
-            binding.imageOrientation.setText(imageOrientation)
-            binding.maker.setText(maker)
-            binding.model.setText(model)
-            binding.stripOffsets.setText(stripOffsets)
-            binding.gpsVersionID.setText(gpsVersionID)
-            binding.gpsLatitude.setText(gpsLatitude)
-            binding.gpsLongitude.setText(gpsLongitude)
-            binding.dateTimeOriginal.setText(dateTimeOriginal)
-            binding.changeDateAndTime.setText(dateTime)
 
-            // セット -> exifInterface.setAttribute(ExifInterface.<TAG>, <value>)
-            // セーブ -> exifInterface.saveAttributes()
-            exifInterface.saveAttributes()
+
+                // 画像の高さ
+                var imageLength = exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH)
+                // 画像の横幅
+                var imageWidth = exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH)
+                // 画像のビットの深さ
+                var bitsPerSample = exifInterface.getAttribute(ExifInterface.TAG_BITS_PER_SAMPLE)
+                // 圧縮の種類
+                var compression = exifInterface.getAttribute(ExifInterface.TAG_COMPRESSION)
+                // 画像タイトル
+                var imageDescription =
+                    exifInterface.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION)
+                // 画像方向
+                var imageOrientation = exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION)
+                // メーカ名
+                var maker = exifInterface.getAttribute(ExifInterface.TAG_MAKE)
+                //モデル名
+                var model = exifInterface.getAttribute(ExifInterface.TAG_MODEL)
+                // ロケーション
+                var stripOffsets = exifInterface.getAttribute(ExifInterface.TAG_STRIP_OFFSETS)
+                // GPSタグのバージョン
+                var gpsVersionID = exifInterface.getAttribute(ExifInterface.TAG_GPS_VERSION_ID)
+                // 経度
+                var gpsLatitude = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE)
+                // 緯度
+                var gpsLongitude = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)
+                // 原画像データの生成日時
+                var dateTimeOriginal =
+                    exifInterface.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL)
+                // 更新日時
+                var dateTime = exifInterface.getAttribute(ExifInterface.TAG_DATETIME)
+
+                // データセット
+                binding.imageLength.setText(imageLength)
+                binding.imageWidth.setText(imageWidth)
+                binding.bitsPerSample.setText(bitsPerSample)
+                binding.compression.setText(compression)
+                binding.imageDescription.setText(imageDescription)
+                binding.imageOrientation.setText(imageOrientation)
+                binding.maker.setText(maker)
+                binding.model.setText(model)
+                binding.stripOffsets.setText(stripOffsets)
+                binding.gpsVersionID.setText(gpsVersionID)
+                binding.gpsLatitude.setText(gpsLatitude)
+                binding.gpsLongitude.setText(gpsLongitude)
+                binding.dateTimeOriginal.setText(dateTimeOriginal)
+                binding.changeDateAndTime.setText(dateTime)
+
+                // セット -> exifInterface.setAttribute(ExifInterface.<TAG>, <value>)
+                // セーブ -> exifInterface.saveAttributes()
+                exifInterface.saveAttributes()
+            }
 
         } catch (e: IOException) {
             e.stackTrace
             e.message?.let { Log.e("ExifActivity", it) }
-
-        } finally {
-            if (`in` != null) {
-                try {
-                    `in`.close()
-                } catch (ignored: IOException) {
-                }
-            }
         }
         return binding.root
     }
--- app/src/main/java/com/example/exif/MainActivity.kt
+++ app/src/main/java/com/example/exif/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.exif
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
@@ -19,12 +28,34 @@ class MainActivity : AppCompatActivity() {
 
     //フィールドの記載
     private var imageRecycler: RecyclerView? = null
-    private var progressBar: ProgressBar?=null
-    private var allPictures:ArrayList<Image>?= null
+    private var progressBar: ProgressBar? = null
+    private var allPictures: ArrayList<Image>? = null
 
+    // 自動再起動用の変数
+    private var context: Context? = null
+    private var waitperiod = 0
+    private var flag: Boolean? = false
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // すべてにファイルを管理できるアクセス権限を付与（Exifの編集に必要）
+        // 許可しない限りアプリが使用できない
+        if (Environment.isExternalStorageManager()) {
+            //todo when permission is granted
+        } else {
+            //request for the permission
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            val uri: Uri = Uri.fromParts("package", packageName, null)
+            flag = true
+            intent.data = uri
+            startActivity(intent)
+        }
+
         val navController = findNavController(R.id.nav_host_fragment)
         setupWithNavController(bottom_navigation, navController)
 
@@ -32,50 +63,54 @@ class MainActivity : AppCompatActivity() {
         imageRecycler = findViewById(R.id.image_recycler)
         progressBar = findViewById(R.id.recycler_progress)
         //リサイクルビューのグリットレイアウトで表示されている画像の制御、spanCountは4列で画像を並べてる意味
-        imageRecycler?.layoutManager=GridLayoutManager(this,4)
+        imageRecycler?.layoutManager = GridLayoutManager(this, 4)
         //これで表示画像の大きさを均等になるよう修正を加えている。falseにしたら大変な事になる。
         imageRecycler?.setHasFixedSize(true)
 
-        val permissions = arrayOf(
-            android.Manifest.permission.READ_EXTERNAL_STORAGE,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            android.Manifest.permission.ACCESS_MEDIA_LOCATION,
-            android.Manifest.permission.MANAGE_EXTERNAL_STORAGE,
-        )
-        ActivityCompat.requestPermissions(this, permissions, 0)
+//        // メディアへのアクセス権限を付与
+//        if(ContextCompat.checkSelfPermission(
+//                this@MainActivity,
+//                Manifest.permission.READ_EXTERNAL_STORAGE
+//            )!= PackageManager.PERMISSION_GRANTED
+//        ){
+//            ActivityCompat.requestPermissions(
+//                this@MainActivity,
+//                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),101
+//            )
+//        }
 
-        allPictures= ArrayList()
+        allPictures = ArrayList()
 
-        if(allPictures!!.isEmpty())
-        {
-            progressBar?.visibility= View.VISIBLE
+        if (allPictures!!.isEmpty()) {
+            progressBar?.visibility = View.VISIBLE
             //画像取得の際のプログレスバーの不可視設定かつimageRecyclerに
             // allpicturesの画像配列をセット。
-            allPictures=getAllImages()
+            allPictures = getAllImages()
             //Adapterをリサイクラーにセットする
-            imageRecycler?.adapter= PhotoAdapter(this,allPictures!!)
-            progressBar?.visibility=View.GONE
+            imageRecycler?.adapter = PhotoAdapter(this, allPictures!!)
+            progressBar?.visibility = View.GONE
         }
 
     }
+
     //外部ストレージからすべての画像を取得するメソッドの設定
-    private fun getAllImages(): ArrayList<Image>?{
+    private fun getAllImages(): ArrayList<Image>? {
         val images = ArrayList<Image>()
         val allImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val projection =
-                arrayOf(MediaStore.Images.ImageColumns.DATA, MediaStore.Images.Media.DISPLAY_NAME)
+            arrayOf(MediaStore.Images.ImageColumns.DATA, MediaStore.Images.Media.DISPLAY_NAME)
 
         var cursor =
-                this@MainActivity.contentResolver.query(allImageUri, projection, null, null, null)
+            this@MainActivity.contentResolver.query(allImageUri, projection, null, null, null)
 
         try {
             cursor!!.moveToFirst()
             do {
                 val image = Image()
                 image.imagePath =
-                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
                 image.imageName =
-                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
                 images.add(image)
             } while (cursor.moveToNext())
             cursor.close()
@@ -83,5 +118,45 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
         return images
+    }
+
+
+    private fun restart(cnt: Context, period: Int) {
+        // intent 設定で自分自身のクラスを設定
+        val mainActivity = Intent(cnt, MainActivity::class.java)
+
+        // PendingIntent , ID=0
+        val pendingIntent = PendingIntent.getActivity(
+            cnt,
+            0, mainActivity, PendingIntent.FLAG_CANCEL_CURRENT
+        )
+
+        // AlarmManager のインスタンス生成
+        val alarmManager = cnt.getSystemService(
+            Context.ALARM_SERVICE
+        ) as AlarmManager
+
+        // １回のアラームを現在の時間からperiod（x秒）後に実行させる
+        if (alarmManager != null) {
+            val trigger = System.currentTimeMillis() + period
+            alarmManager.setExact(AlarmManager.RTC, trigger, pendingIntent)
+        }
+
+        // アプリ終了
+        finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // 権限を付与後、再起動しないとメディアから読み込まれないため、自動で再起動にするように設定したが
+        // 上手くいかないため、権限を変更後は自動で停止するようにした。
+        // （スリープ時間を限りなく大きくすることで）
+        if (flag == true) {
+            context = applicationContext
+            waitperiod = 100000000
+            context?.let { restart(it, waitperiod) }
+            flag = false
+        }
     }
 }
